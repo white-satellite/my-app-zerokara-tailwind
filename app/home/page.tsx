671656a8a5a0b651e200df6edbfaeb1f1ef20677
white-satellite <masayukiuminin8@gmail.com>
2024-05-08 20:02:18 +0900

    要素を真ん中に置く

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="py-24 mx-auto max-w-md">
+            <p className="tracking-wide">Hello mily!!!</p>
+        </div>
+        
+    )
+}
